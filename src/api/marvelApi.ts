6ad783be54222a60c7526c3ffa9586309a86d390
BMR11 <brewmerang@gmail.com>
2022-01-31 23:28:23 -0600

    removing marvel api creds

--- src/api/marvelApi.ts
+++ src/api/marvelApi.ts
@@ -3,8 +3,8 @@ import md5 from 'md5';
 import {RECORD_COUNT_PER_FETCH} from '../screens/ComicList/ComicListScreen';
 
 const ts = 'marvel-api';
-const publicKey = '764d0a59d47c833643c3582985e255ed';
-const privateKey = '4e113fe79bdbc144bdc414c9d57ca3f91a40fdb3';
+const publicKey = 'YOUR_PUBLIC_KEY';
+const privateKey = 'YOUR_PRIVATE_KEY';
 const hash = md5(`${ts}${privateKey}${publicKey}`);
 
 const apiKey = {
